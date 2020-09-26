72ccbd95470dedfa3bc9bc1f0793ba339bf4ce0e
David Coe <davidcoe@microsoft.com>
2020-09-26 10:36:44 -0400

    json2

--- csv_json.py
+++ csv_json.py
@@ -4,7 +4,15 @@ import json
 
 df =pd.read_csv('https://raw.githubusercontent.com/davidhcoe/mads_591_592/master/data/NBER/cbsa2fipsxw.csv')
 
-j = df.to_json()
+j = df.to_json(orient='records')
 
-with open('nber.json','w') as file:
+with open('nber2.json','w') as file:
     file.write(j)
+
+#df2 = pd.read_json('https://raw.githubusercontent.com/davidhcoe/mads_591_592/master/data/NBER/cbsa2fipsxw.json')
+
+df2 = pd.read_json(j)
+
+print(df.iloc[3])
+print('---')
+print(df2.iloc[3])
